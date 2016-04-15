b27f9e68be3976c63d1a97a3bc846180260c085f
Bvnkame <bvnbase@gmail.com>
2016-04-16 02:58:02 +0700

    - Add new funcs

--- CloudScript.js
+++ CloudScript.js
@@ -1,3 +1,8 @@
+function FunkoiResponseFormat(result)
+{
+	return JSON.stringify(result).replace(/"/g, '\\"');
+}
+
 handlers.HelloWorld = function(args){ 
 	var result = {}; 
 	result.message = "hello world"; 
@@ -17,4 +22,19 @@ handlers.GetTime = function(args){
 	var result = {}; 
 	result.timeInSeconds = n;
 	return JSON.stringify(result).replace(/"/g, '\\"');
+}
+
+handlers.SendPushNotification = function(args)
+{
+	var request {
+		Recipient : 7A2290C930139A65,
+		Message : "Hello, nice to meet you",
+	};
+
+	server.SendPushNotification(request);
+
+	var result = {}; 
+	result.message = "sent";
+
+	return FunkoiResponseFormat(result);
 }
