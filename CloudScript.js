7ddd0b8180dbde3c91b9dff68884b8d6735fb338
Bùi Văn Nam <bvnbase@gmail.com>
2016-04-15 17:24:02 +0700

    Update CloudScript.js

--- CloudScript.js
+++ CloudScript.js
@@ -9,3 +9,12 @@ handlers.Hello = function(args){
 	result.message = "hello"; 
 	return JSON.stringify(result).replace(/"/g, '\\"');
 }
+
+handlers.GetTime = function(args){
+	var d = new Date();
+	var n = d.getTime();
+	
+	var result = {}; 
+	result.timeInSeconds = n;
+	return JSON.stringify(result).replace(/"/g, '\\"');
+}
