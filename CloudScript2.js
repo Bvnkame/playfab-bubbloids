17b811e63c9ace78a965630a4234d88cb74e9314
Bvnkame <bvnbase@gmail.com>
2016-04-16 02:37:59 +0700

    - Push

---
+++ CloudScript2.js
@@ -0,0 +1,8 @@
+handlers.GetTime = function(args){
+	var d = new Date();
+	var n = d.getTime();
+	
+	var result = {}; 
+	result.timeInSeconds = n;
+	return JSON.stringify(result).replace(/"/g, '\\"');
+}
