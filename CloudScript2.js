919342cc68a9a1363085eda943377f3f642a41bd
Bvnkame <bvnbase@gmail.com>
2016-04-16 02:43:08 +0700

    - Remove one

--- CloudScript2.js
+++
@@ -1,8 +0,0 @@
-handlers.GetTime = function(args){
-	var d = new Date();
-	var n = d.getTime();
-	
-	var result = {}; 
-	result.timeInSeconds = n;
-	return JSON.stringify(result).replace(/"/g, '\\"');
-}
