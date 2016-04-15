db0aad0cc7b523a286a77bc31a76f8b9cb292d94
Bùi Văn Nam <bvnbase@gmail.com>
2016-04-15 16:54:01 +0700

    Update CloudScript.js

--- CloudScript.js
+++ CloudScript.js
@@ -1,11 +1,11 @@
 handlers.HelloWorld = function(args){ 
 	var result = {}; 
 	result.message = "hello world"; 
-	return JSON.stringify(result).replace(/\"/g,'&quot;');;
+	return JSON.stringify(result).replace(/"/g, '\\"');
 }
 
 handlers.Hello = function(args){ 
 	var result = {}; 
 	result.message = "hello"; 
-	return JSON.stringify(result).replace(/\"/g,'&quot;');;
+	return JSON.stringify(result).replace(/"/g, '\\"');
 }
